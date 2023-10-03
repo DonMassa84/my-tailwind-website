83320e90ad05446e9790217f662b1878f048e6cd
Maximilian Trustworth <100371773+DonMassa84@users.noreply.github.com>
2023-10-04 01:14:49 +0200

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+// components/Header.tsx
+const Header = () => {
+  return (
+    <header className="bg-blue-500 py-4 text-white text-center">
+      <h1 className="text-2xl font-semibold">My Tailwind Website</h1>
+    </header>
+  );
+};
+
+export default Header;
+
